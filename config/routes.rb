82b669cf2107c82b5fce4365a67c508fb387463e
EvyOliveira <cristinioliveira.eo@gmail.com>
2020-10-16 11:52:29 -0300

    Refactor profile page * users_controller.rb change the mothod show to receive the current user instead the user id * profile.html.erb rename the file to show.html.erb * routes.rb create the resource to receive the method show for users

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   get   '/auth/failure',                      to: 'sessions#failure'
   get   '/logout',                            to: 'sessions#destroy', via: [:get, :post]
   get   '/signup',                            to: 'identities#new' # registering user with omniauth-identity
-  get   '/profile' ,                          to: 'users#profile'
 
   resources :identities
   resources :password_resets
+  resources :users, only: [:show]
 
   # Gives letter_opener an interface for browsing sent emails: http:localhost:3000/letter_opener
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
