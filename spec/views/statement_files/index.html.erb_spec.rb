dfdfaa1ae42e21c1c178fb392380188bfecd83c6
Rodolfo Porfirio <rodolforpr88@gmail.com>
2020-10-23 14:51:24 -0300

    remove all the unnused and blank tests

--- spec/views/statement_files/index.html.erb_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "statement_files/index", type: :view do
-  let!(:statement_file) { create(:statement_file, :with_attachment) } 
-
-  it "renders a list of statement_files"
-end
