155c17a0a770ba50917054401bf237788efac2ca
Rodolfo Porfirio <rodolforpr88@gmail.com>
2020-10-26 11:38:29 -0300

    remove unnused texts and refactor the flash messages * remove some comments unnused * change the errors call to 'alert' and 'notice' instead of 'errors' and 'success' * fix the call for errors using '@statement.errors.full_messages' * update the statement_params to receive statement files id and brokerage account id * refecator the redirects to use 'path' in the end instead of 'url'

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -1,71 +1,56 @@
 class StatementsController < ApplicationController
-  before_action :set_statement, only: [:show, :edit, :update, :destroy]
+  before_action :set_statement, only: [:show, :edit, :destroy]
 
-  # GET /statements
-  # GET /statements.json
   def index
     @statements = Statement.all
   end
 
-  # GET /statements/1
-  # GET /statements/1.json
   def show
   end
 
-  # GET /statements/new
   def new
     @statement = Statement.new
   end
 
-  # GET /statements/1/edit
   def edit
   end
 
-  # POST /statements
-  # POST /statements.json
   def create
     begin
     @statement = Statement.new(statement_params)
-
       if @statement.save
-        flash['success']  = "statement was successfully created."
+        flash[:notice]  = "Statement was successfully created."
       else
-        flash['errors'] = "statement can't be blank."
+        flash[:alert] = "#{@statement.errors.full_messages}"
       end
     rescue ActionController::ParameterMissing => exception
-      flash['errors'] = "statement can't be blank."
+      flash[:alert] = "Statement can't be blank."
     end
   end
 
-  # PATCH/PUT /statements/1
-  # PATCH/PUT /statements/1.json
   def update
     if @statement.update(statement_params)
-      flash['success']  = 'Statement was successfully updated.'
+      flash[:notice]  = 'Statement was successfully updated.'
     else
-      flash['errors'] = "#{@statement.errors}"
+      flash[:alert] = "#{@statement.errors.full_messages}"
     end
-    redirect_to statement_files_url
+    redirect_to statement_files_path
   end
 
-  # DELETE /statements/1
-  # DELETE /statements/1.json
   def destroy
-    @statement.destroy
-    respond_to do |format|
-      format.html { redirect_to statements_url, notice: 'Statement was successfully destroyed.' }
-      format.json { head :no_content }
+    if @statement.destroy
+      redirect_to statements_path, notice: 'Statement was successfully destroyed.'
+    else
+      redirect_to statements_path, alert: 'Statement was not destroyed.'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_statement
       @statement = Statement.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def statement_params
-      params.require(:statement).permit(:statement_date, :statement_file_id)
+      params.require(:statement).permit(:statement_date, :statement_file_id, :brokerage_account_id)
     end
 end
