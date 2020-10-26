18cde9bd6c23217e4c5f265c98b6e737c2ad6b63
Rodolfo Porfirio <rodolforpr88@gmail.com>
2020-10-26 12:11:59 -0300

    refactor the destroy method and remove some comments * add the before action for destroy * add the set_statement_file to use in before action * update the 'destroy' method using that in the 'if/else' block

--- app/controllers/statement_files_controller.rb
+++ app/controllers/statement_files_controller.rb
@@ -1,6 +1,6 @@
 class StatementFilesController < ApplicationController
-  # GET /statement_files
-  # GET /statement_files.json
+  before_action :set_statement_file, only: [:destroy]
+
   def index
     @statement_files = StatementFile.all
   end
@@ -8,9 +8,7 @@ class StatementFilesController < ApplicationController
   def new
     @statement_file = StatementFile.new
   end
-  
-  # POST /statement_files
-  # POST /statement_files.json
+
   def create
     begin
       @statement_file = StatementFile.new(statement_file_params)
@@ -20,25 +18,26 @@ class StatementFilesController < ApplicationController
       else
         redirect_to statement_files_path, alert: 'Statement file was not created, please try again.'
       end
-
     rescue ActionController::ParameterMissing => exception
       redirect_to statement_files_path, alert: 'Statement File must be present.'
     end
   end
 
-  # DELETE /statement_files/1
-  # DELETE /statement_files/1.json
   def destroy
-    @statement_file = StatementFile.find(params[:id])
-
-    @statement_file.destroy
-
-    redirect_to statement_files_path, notice: 'Statement file was successfully destroyed.'
+    if @statement_file.destroy
+      redirect_to statement_files_path, notice: 'Statement file was successfully destroyed.'
+    else
+      redirect_to statement_files_path, alert: 'Statement file was not destroyed.'
+    end
   end
 
   private
-    # Only allow a list of trusted parameters through.
-    def statement_file_params
-      params.require(:statement_file).permit(:file)
-    end
+
+  def set_statement_file
+    @statement_file = StatementFile.find(params[:id])
+  end
+
+  def statement_file_params
+    params.require(:statement_file).permit(:file, :account_id)
+  end
 end
