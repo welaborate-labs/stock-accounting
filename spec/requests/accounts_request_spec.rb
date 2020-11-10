e90294268ddb626425eb35bcd7a4f2bed623d7e6
rodolforpr88 <rodolforpr88@gmail.com>
2020-11-10 15:15:30 -0300

    add 'index&show' and refactor the flash messages in accounts_request_spec.rb

--- spec/requests/accounts_request_spec.rb
+++ spec/requests/accounts_request_spec.rb
@@ -4,6 +4,20 @@ RSpec.describe "Accounts", type: :request do
   let(:user) { create(:user) }
   let(:account) { create(:account, user_id: user.id) }
 
+  describe "GET #index" do
+    before { get accounts_path }
+
+    it { is_expected.to render_template("index") }
+    it { expect(assigns(:accounts)).to eq([account]) }
+  end
+
+  describe "GET #show" do
+    before { get account_path(account) }
+
+    it { expect(assigns(:account)).to eq(account) } 
+    it { expect(response.body).to include account.name.to_s } 
+  end
+
   describe "GET #new" do
     before { get new_account_path }
 
@@ -17,8 +31,8 @@ RSpec.describe "Accounts", type: :request do
     it { is_expected.to render_template("edit") }
     it { expect(assigns(:account)).to eq(account) }
   end
-
-  describe "GET #create" do
+  
+  describe "POST #create" do
     context "valid attributes" do
       before { post accounts_path, params: { account: { user_id: user.id, document: 'abc', name: 'def' }}}
       subject { post accounts_path, params: { account: { user_id: user.id, document: 'abc', name: 'def' }}}
@@ -32,7 +46,7 @@ RSpec.describe "Accounts", type: :request do
       before { post accounts_path, params: { account: { user_id: nil, document: nil, name: nil }}}
       subject { post accounts_path, params: { account: { user_id: nil, document: nil, name: nil }}} 
 
-      it { expect(flash[:alert]).to eq("User must exist, User can't be blank, Name can't be blank, and Document can't be blank") }
+      it { expect(flash[:alert]).to eq(["User must exist", "User can't be blank", "Name can't be blank", "Document can't be blank"])}
       it { expect {subject}.not_to change(Account, :count) }
     end
   end
@@ -58,7 +72,7 @@ RSpec.describe "Accounts", type: :request do
       it { expect(account.name).to eq('MyString') }
       it { expect(account.name).not_to eq(nil) }
       it { is_expected.to redirect_to new_account_path }
-      it { expect(flash[:alert]).to eq("Name can't be blank") }
+      it { expect(flash[:alert]).to eq(["Name can't be blank"]) }
     end
   end
 end
