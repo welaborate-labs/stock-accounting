8bb06e96bff12bc0843204de9ccfcb67fd3ce3a6
rodolforpr88 <rodolforpr88@gmail.com>
2020-11-11 11:47:09 -0300

    create the destroy action and refactor the redirects

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,8 +1,9 @@
 class AccountsController < ApplicationController
-  before_action :set_account, only: [:edit, :update, :show]
+  before_action :set_account, only: [:edit, :update, :show, :destroy]
 
   def index
-    @accounts = Account.all
+    # TODO need to mock the current_user to pass the test
+    @accounts = Account.where(user_id: current_user)
   end
 
   def new
@@ -11,20 +12,29 @@ class AccountsController < ApplicationController
 
   def create
     @account = Account.new(account_params)
+    #TODO need to uncomment for test pass
     @account.user_id = current_user.id
 
     if @account.save
-      redirect_to new_account_path, notice: 'Account was successfully created.'
+      redirect_to accounts_path, notice: 'Account was successfully created.'
     else
-      redirect_to new_account_path, alert: @account.errors.full_messages
+      redirect_to accounts_path, alert: @account.errors.full_messages
     end
   end
 
   def update
     if @account.update(account_params)
-      redirect_to new_account_path, notice: 'Account was successfully updated.'
+      redirect_to accounts_path, notice: 'Account was successfully updated.'
     else
-      redirect_to new_account_path, alert: @account.errors.full_messages
+      redirect_to accounts_path, alert: @account.errors.full_messages
+    end
+  end
+
+  def destroy
+    if @account.destroy
+      redirect_to accounts_path, notice: 'Account was successfully deleted.'
+    else
+      redirect_to accounts_path, notice: 'Account was not updated.'
     end
   end
 
