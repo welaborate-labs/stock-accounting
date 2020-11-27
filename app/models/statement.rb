299397d6cbcda0cc7e9d500c45b3c0cbe57b097b
rodolforpr88 <rodolforpr88@gmail.com>
2020-11-27 15:33:49 -0300

    add the validations to the methods

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -9,13 +9,16 @@ class Statement < ApplicationRecord
   private
 
     def set_statement_date_from_content
-      self.statement_date = content.match('\d{1,2}\/\d{1,2}\/\d{4}').to_s
+      if content
+        self.statement_date = content.match('\d{1,2}\/\d{1,2}\/\d{4}').to_s
+      end
     end
 
     def set_brokerage_account_from_content
-      client_number  = content.match('\d{7}\-?\d{1}').to_s
-      account        = statement_file.account
-
-      self.brokerage_account = account.brokerage_accounts.find_or_create_by(brokerage: 1, number: client_number)
+      if content && statement_file
+        client_number = content.match('\d{7}\-?\d{1}').to_s
+        account = statement_file.account
+        self.brokerage_account = account.brokerage_accounts.find_or_create_by(brokerage: 1, number: client_number)
+      end
     end
 end
