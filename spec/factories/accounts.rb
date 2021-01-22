6af0d7e0d5a2fd88e8ba6062ec1db2b341591470
rodolforpr88 <rodolforpr88@gmail.com>
2021-01-22 11:06:21 -0300

    update the account's factory with valid informations, also refactor other changes in the actions 'create, update and choose' to handle the changes in the controller and model

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :account do
     user
-    name { "MyString" }
-    document { "MyString" }
-    address { "MyText" }
-    address_complement { "MyText" }
-    city { "MyString" }
-    state { "MyString" }
-    country { "MyString" }
-    zipcode { "MyString" }
-    phone_personal { "MyString" }
-    phone_business { "MyString" }
-    phone_mobile { "MyString" }
-    status { "MyString" } 
+    name { "MyFactoryName" }
+    document { "12345678910" }
+    address { "MyFactoryAddress MyFactoryAddress2" }
+    address_complement { "MyFactoryComplement" }
+    city { "MyFactoryCity" }
+    state { "MyFactoryState" }
+    country { "MyFactoryCountry" }
+    zipcode { "12345678" }
+    phone_personal { "1147474747" }
+    phone_business { "1247474748" }
+    phone_mobile { "13998765432" }
+    status { "Active" } 
   end
 end
