a2fc76bccb570cff46ed934702b5632e13090029
rodolforpr88 <rodolforpr88@gmail.com>
2021-02-24 17:03:51 -0300

    update the cust per trade method and the respective rspec

--- spec/services/custody_service_spec.rb
+++ spec/services/custody_service_spec.rb
@@ -15,16 +15,18 @@ RSpec.describe Custody do
     before { allow_any_instance_of(ApplicationController).to receive(:choosen_account) { account } }
     subject { Custody.trades_custody }
     context "should return the open trades" do
-      it { expect(subject).to include [trade, trade2], [trade4] }
+      it { expect(subject).to include trade, trade2, trade4 }
       
     end
     context "should not return the close trades" do
-      it { expect(subject).not_to include [trade3], [trade5] }
+      it { expect(subject).not_to include trade3, trade5 }
       
     end
   end
 
   describe "returns the avg price" do
-    it { expect(Custody.avg_cust(trade)).to be 10.0 }
+    it { expect(Custody.avg_cust).to eq [[[trade], [30.0]], 
+                                         [[trade2], [30.0]], 
+                                         [[trade4], [30.0]]] }
   end
 end
