36d4bd65c1f279a85091b958275293e12826f16c
Filipe Abreu <filabreu@gmail.com>
2021-03-29 10:49:59 -0300

    Add has many Trades through Statements to BrokerageAccount model

--- app/models/brokerage_account.rb
+++ app/models/brokerage_account.rb
@@ -1,5 +1,6 @@
 class BrokerageAccount < ApplicationRecord
   belongs_to :account
   has_many :statements, dependent: :destroy
+  has_many :trades, through: :statements
   validates_presence_of :account, :brokerage, :number
 end
