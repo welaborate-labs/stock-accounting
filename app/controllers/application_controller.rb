e8c1fafb74fc7cdaaf55c07162339e6c5852ba07
rodolforpr88 <rodolforpr88@gmail.com>
2021-04-01 15:51:48 -0300

    add 4 methods to handle the payment integration with the Vindi API * Payment_api - create the global variable of the linking with the Vindi API and another with all the customers * Set_customer - save in a global variable all the register of a especific key 'EMAIL' in this case * Check_subscription - checks if at least one of the variable @customer is 'active' and returns 'true/'false' * create the method to link to the customer choose the plan and add the payment informations
    Add the before_action :check_subscription! that first checks the choosen_account, after if the customer is 'active' and if isn't will redirect to the home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
+  before_action :check_subscription!
   helper_method :current_user, :signed_in?, :choosen_account, :check_choosen_account, :set_last_account
+  helper_method :check_subscription?
   include Pagy::Backend
+
+  def plans_route
+    redirect_to 'https://sandbox-app.vindi.com.br/customer/pages/390f8270-ec63-45a5-a359-373ef8886343/subscriptions/new'
+  end
   
   protected
 
@@ -21,6 +27,11 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user!
     redirect_to root_path unless signed_in?
+  end
+
+  def check_subscription!
+    return if check_choosen_account
+    redirect_to home_path unless check_subscription?
   end
 
   def choosen_account
@@ -45,5 +56,23 @@ class ApplicationController < ActionController::Base
       choosen_account = current_user.accounts.last
       session[:choosen_account_id] = choosen_account.id
     end
+  end
+
+  def set_customer
+    @customers.map do |customer|
+      customer if customer[:email].eql? current_user.email
+    end.compact
+  end
+
+  def check_subscription?
+    payment_api
+    set_customer.each do |register|
+      return register[:status].eql? 'active'
+    end unless set_customer.empty?
+  end
+
+  def payment_api
+    @vindi_api = Vindi::Client.new(key: ENV['VINDI_SANDBOX_KEY'], api_endpoint: 'https://sandbox-app.vindi.com.br/api/v1/')
+    @customers = @vindi_api.list_customers
   end
 end
